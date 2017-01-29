a0531b011318b7626b24439e94a4ce8b19cfaf4b
nagen-mac <nagen@mac.com>
2017-01-29 09:24:31 -0500

    renamed:    database.py -> datamining-bot/database.py 	modified:   datamining-bot/moviesExtract.py 	modified:   datamining-bot/navaras_dev.db :x!

--- datamining-bot/moviesExtract.py
+++ datamining-bot/moviesExtract.py
@@ -14,11 +14,12 @@ dbsession = DBSession()
 
 #get the urls for telugu movies - https://en.wikipedia.org/wiki/Lists_of_Telugu-language_films - complete
 #get the urls for BOllywood movies - https://en.wikipedia.org/wiki/Lists_of_Bollywood_films
+#https://en.wikipedia.org/wiki/List_of_Bollywood_films_of_1962
 # fix for lxml issue - STATIC_DEPS=true sudo pip install lxml
 
 def getalllinks():
     urls = []
-    html = urlopen("https://en.wikipedia.org/wiki/Lists_of_Telugu-language_films")
+    html = urlopen("https://en.wikipedia.org/wiki/Lists_of_Bollywood_films")
     bsObj = BeautifulSoup(html, "html.parser")
 
     print("1 - Collecting links.....")
@@ -26,14 +27,12 @@ def getalllinks():
     for link in bsObj.find("div", {"id":"mw-content-text"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$")):
         if 'href' in link.attrs:
             temp = link.attrs['href']
-            if 'wiki/List_of_Telugu_films_of_' in temp and temp not in urls \
-                and 'wiki/List_of_Telugu_films_of_the_' not in temp:
+            if 'wiki/List_of_Bollywood_films_of_' in temp and temp not in urls \
+                and 'wiki/List_of_Bolloywood_films_of_the_' not in temp:
                 urls.append("https://en.wikipedia.org" + temp)
 
     print("2 - Links Collection complete.")
     return urls
-
-
 
 #Scrap the HTML for tables in each url in the urls list
 def getMoviesData(url):
@@ -56,51 +55,51 @@ def getMoviesData(url):
                         tableheader.append(header.text)
 
                     tablerowdata = []
+                    movieUrl = ''
+                    movietitle = ''
                     for row in rows.find_all('td'):
 
+                        for tag in row.find_all('i'):
+                            movietitle = tag.text
+
+                            for atag in tag.find_all('a'):
+                                if 'index.php' not in atag['href']:
+                                    movieUrl = atag['href']
+
+                        if movietitle:
+                            tablerowdata.append(movietitle)
+                            movietitle = ''
                         if 'rowspan' not in row.attrs and 'style' not in row.attrs and 'id' not in row.attrs and row is not None:
-                            for tag in row.find_all('i'):
-                                movietitle = tag.text
-                                movieUrl = ''
+                            tablerowdata.append(row.text)
 
-                                for atag in tag.find_all('a'):
-                                    if 'index.php' not in atag['href']:
-                                        movieUrl = atag['href']
-
-                                if movieUrl:
-                                    tablerowdata.append(movieUrl)
-                                    movieUrl = ''
-                                else:
-                                    tablerowdata.append('None')
-
-                            if movietitle:
-                                tablerowdata.append(movietitle)
-                                movietitle = ''
-                            else:
-                                tablerowdata.append(row.text)
-
-                    year = re.findall('\d+', url)
                     if tablerowdata:
-                        if 'Open' in tableheader[0]:
+                        if tablerowdata[0] == tablerowdata[1]:
+                            tablerowdata.pop(0)
+                        if 'Open' in tableheader[0] or '' == tableheader[0] or 'Starting' in tableheader[0]:
                             tableheader.pop(0)
                         movie = Movies()
-                        movie.ref = tablerowdata[0]
-                        movie.title = tablerowdata[1]
-                        movie.directedBy = tablerowdata[2]
-                        movie.starringBy = tablerowdata[3]
-                        if tableheader[3] == 'Genre':
-                            movie.genre = tablerowdata[4]
-                        if 'Music' in tableheader[3]:
-                            movie.musicBy = tablerowdata[4]
-                        if len(tablerowdata) > 4:
-                            if 'Music' in tableheader[4] and tablerowdata[5]:
-                                movie.musicBy = tablerowdata[5]
-                            elif 'Produ' in tableheader[4] or 'Note' in tableheader[4]:
-                                movie.producedBy = tablerowdata[5]
-                            if len(tableheader) > 5 and len(tablerowdata) > 5:
-                                if 'Produ' in tableheader[5] or 'Notes' in tableheader[5]:
-                                    movie.producedBy = tablerowdata[6]
+
+                        if 'Title' in tableheader[0]:
+                            movie.title = tablerowdata[0]
+                        if 'Direct' in tableheader[1]:
+                            movie.directedBy = tablerowdata[1]
+                        if 'Cast' in tableheader[2]:
+                            movie.starringBy = tablerowdata[2]
+
+                        if len(tableheader) > 3 or tablerowdata > 3:
+                            if 'Genre' in tableheader[3]:
+                                movie.genre = tablerowdata[3]
+                            if 'Music' in tableheader[3]:
+                                movie.musicBy = tablerowdata[3]
+                        if len(tableheader) > 4:
+                            if 'Music' in tableheader[4] and len(tablerowdata) > 4:
+                                movie.musicBy = tablerowdata[4]
+                            elif 'Produ' in tableheader[4] or 'Notes' in tableheader[4] and len(tablerowdata) > 4:
+                                movie.producedBy = tablerowdata[4]
+                        year = re.findall('\d+', url)
                         movie.year = year[0]
+                        movie.ref = movieUrl
+                        movie.language_id = 2
                         #dbsession.add(movie)
                         #dbsession.commit()
     except AttributeError as e:
@@ -110,19 +109,12 @@ def getMoviesData(url):
 
     return None
 
-url = 'https://en.wikipedia.org/wiki/List_of_Telugu_films_of_1961'
-data = getMoviesData(url)
+#url = 'https://en.wikipedia.org/wiki/List_of_Telugu_films_of_2017'
+#data = getMoviesData(url)
 
-#header = "Title,Director,Cast,Genre, Production House"
-#file = open(r"/Users/nagen/Documents/Python-Workspace/wiki-scraping/output.csv", "wb")
-#file.write(bytes(header, encoding="UTF-8", errors="ignore"))
+teluguurls = getalllinks()              #["https://en.wikipedia.org/wiki/List_of_Telugu_films_of_1990", "https://en.wikipedia.org/wiki/List_of_Telugu_films_of_2000"]
+telugu_yr_urls = list(set(teluguurls))  # pick only unique values from list
+telugu_yr_urls.sort()                   # sort the Urls in the list
 
-#teluguurls = getalllinks()              #["https://en.wikipedia.org/wiki/List_of_Telugu_films_of_1990", "https://en.wikipedia.org/wiki/List_of_Telugu_films_of_2000"]
-#telugu_yr_urls = list(set(teluguurls))  # pick only unique values from list
-#telugu_yr_urls.sort()                   # sort the Urls in the list
-
-#for url in telugu_yr_urls:
-#    data = getMoviesData(url)
-    #file.write(bytes(data, encoding="UTF-8", errors="ignore"))
-
-#file.close()
+for url in telugu_yr_urls:
+    data = getMoviesData(url)
